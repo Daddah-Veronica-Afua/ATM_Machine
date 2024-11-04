a8dbfc0518476882a06661f4fb1dd619ccdcd72e
VERONICA DADDAH AFUA <daddahveronica35@gmail.com>
2024-11-03 21:12:06 -1200

    Mini_Project3

--- control_flow_atm.py
+++ control_flow_atm.py
@@ -25,6 +25,43 @@ while Number_of_attempts < 3:
             print('Too many incorrect attempts. Access blocked.')
             exit()
 
+            def ATM_Menu():
+                print("\nATM Menu:")
+                print("1. Check Balance")
+                print("2. Deposit Funds")
+                print("3.Withdraw Funds")
+                print("4. Change PIN")
+                print("5. Exit")
+
+                def Check_Balance():
+                    print(f"Please your current balance is: {Balance}")
+
+                    def Deposit_Funds():
+                        Deposit = int(input("Enter amount to deposit: "))
+                        if amount > 0:
+                            balance += amount
+                            print(f"{amount} deposited. New balance is: {balance}")
+                            else:
+                                print("Invalid amount. Please enter a positive number.")
+
+                                def Change_Pin():
+                                    current_pin = (input("Enter your PIN:"))
+                                    if current_pin == pin:
+                                        new_pin == int(input("Enter a new 4-digit PIN:"))
+                                        confirm_pin = int(input("Confirm new PIN:"))
+                                        if new_pin == confirm_new_pin:
+                                            pin = new_pin
+                                            print("PIN successfully changed.")
+                                            else:
+                                                print("PINs do not match.Try again.")
+                                                else:
+                                                    print("Incorrect current PIN.")
+
+
+
+                    
+
+
 
 
  
